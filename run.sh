ba2135af96cc2ef2026b351baa877daa700c4d9c
Walt-like <2982707973@qq.com>
2023-03-10 10:47:58 +0800

    evaluation

--- run.sh
+++ run.sh
@@ -9,26 +9,26 @@ VAL_LIST=./sample_files/imgs/path.txt
 
 #train
 # CUDA_VISIBLE_DEVICES=0,1,2,3 \
- python3 -u -m torch.distributed.launch --nproc_per_node 4  ./tools/train_val.py \
-     --model_name=resnet18 \
-     --lr  0.01 --epochs 70  --batch-size 128  -j 4 \
-     --output=$OUTPUT_PATH/$TIME \
-     --train_list=$TRAIN_LIST \
-     --val_list=$VAL_LIST \
-     --num_classes=2 \
-     --is_pretrained
-    
-    
-    
-    
-# CUDA_VISIBLE_DEVICES=0,1,2,3 \
-# python3 -u -m torch.distributed.launch --nproc_per_node 1  ./tools/evaluation.py \
+# python3 -u -m torch.distributed.launch --nproc_per_node 4  ./tools/train_val.py \
 #     --model_name=resnet18 \
-#     --batch-size 64  -j 4 \
+#     --lr  0.01 --epochs 70  --batch-size 128  -j 4 \
 #     --output=$OUTPUT_PATH/$TIME \
+#     --train_list=$TRAIN_LIST \
 #     --val_list=$VAL_LIST \
-#     --tune_from='/home/lxztju/pytorch_classification/ouputs/xxx/epoch_4.pth' \
-#     --num_classes=2
+#     --num_classes=2 \
+#     --is_pretrained
+    
+    
+    
+    
+ CUDA_VISIBLE_DEVICES=0,1,2,3 \
+ python3 -u -m torch.distributed.launch --nproc_per_node 1  ./tools/evaluation.py \
+     --model_name=resnet18 \
+     --batch-size 64  -j 4 \
+     --output=$OUTPUT_PATH/$TIME \
+     --val_list=$VAL_LIST \
+     --tune_from='/home/lxztju/pytorch_classification/ouputs/xxx/epoch_4.pth' \
+     --num_classes=2
     
 
 # CUDA_VISIBLE_DEVICES=0,1,2,3 \
